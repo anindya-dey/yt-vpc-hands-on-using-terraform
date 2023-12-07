7d3844a88056978147f6111512627344609f4558
Anindya Dey <andy.ani1992@gmail.com>
2023-12-07 23:12:18 +0530

    remove VPC B

--- 02.create-subnets/vpc.tf
+++ 02.create-subnets/vpc.tf
@@ -8,16 +8,6 @@ resource "aws_vpc" "vpc_a" {
   }
 }
 
-resource "aws_vpc" "vpc_b" {
-  cidr_block = "10.1.0.0/16"
-
-  tags = {
-    Name   = "VPC-B"
-    Author = "Anindya Dey",
-    Reason = "Demo"
-  }
-}
-
 resource "aws_subnet" "public_subnet_vpc_a" {
   vpc_id     = aws_vpc.vpc_a.id
   cidr_block = "10.0.0.0/24"
@@ -35,28 +25,6 @@ resource "aws_subnet" "private_subnet_vpc_a" {
 
   tags = {
     Name = "Private-Subnet-VPC-A"
-    Author = "Anindya Dey",
-    Reason = "Demo"
-  }
-}
-
-resource "aws_subnet" "public_subnet_vpc_b" {
-  vpc_id     = aws_vpc.vpc_b.id
-  cidr_block = "10.1.0.0/24"
-
-  tags = {
-    Name = "Public-Subnet-VPC-B"
-    Author = "Anindya Dey",
-    Reason = "Demo"
-  }
-}
-
-resource "aws_subnet" "private_subnet_vpc_b" {
-  vpc_id     = aws_vpc.vpc_b.id
-  cidr_block = "10.1.1.0/24"
-
-  tags = {
-    Name = "Private-Subnet-VPC-B"
     Author = "Anindya Dey",
     Reason = "Demo"
   }
